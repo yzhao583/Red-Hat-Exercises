ef8e1e917b312296aced8cdc272e4c8769a9f7ce
Yu Zhao <yuzhao@knowroaming.com>
2019-06-16 16:21:37 -0400

    finished exercise three functionalities

--- practical-exercise-three/src/containers/user-detail-container.js
+++ practical-exercise-three/src/containers/user-detail-container.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Component from "../override/component";
 import PropTypes from "prop-types";
-import { Grid, Divider, Segment } from "semantic-ui-react";
+import { Link } from 'react-router-dom';
+import { Grid, Divider, Segment, Dimmer, Loader, Icon } from "semantic-ui-react";
 import Api from "../network/api";
 import BasicTable from "../components/basic-table";
+import Validator from "../utils/validator";
 
 class UserDetailContainer extends Component {
   static propTypes = {
@@ -16,7 +18,8 @@ class UserDetailContainer extends Component {
     this.state = {
       generalInfoList: [],
       addressInfoList: [],
-      companyInfoList: []
+      companyInfoList: [],
+      isLoading: false
     };
   }
 
@@ -24,44 +27,83 @@ class UserDetailContainer extends Component {
   componentDidMount() {
     const { userId } = this.props;
 
+    //turn loader on
+    this.setState({ isLoading: true });
+
+    //Call api to get data
     Api.getUser(userId).then(
+      //store data to state and turn loader off.
       response => {
         this.setState({
           generalInfoList: response.generalInfoList,
           addressInfoList: response.addressInfoList,
-          companyInfoList: response.companyInfoList
+          companyInfoList: response.companyInfoList,
+          isLoading: false
         });
       },
       error => {
         console.err(error);
+
+        //turn loader off
+        this.setState({ isLoading: false });
       }
     );
   }
 
   render() {
-    const { generalInfoList, addressInfoList, companyInfoList } = this.state;
+    const {
+      generalInfoList,
+      addressInfoList,
+      companyInfoList,
+      isLoading
+    } = this.state;
 
-    return (
-      <Segment>
-        <h1>User Details</h1>
-        <Grid className="fade-in">
-          <Grid.Row>
-            <Grid.Column mobile={16} computer={8}>
-              <BasicTable title="General" data={generalInfoList} className="no-bottom-border" />
-            </Grid.Column>
-          </Grid.Row>
-          <Divider />
-          <Grid.Row>
-            <Grid.Column mobile={16} computer={8}>
-              <BasicTable title="Address" data={addressInfoList} className="no-bottom-border" />
-            </Grid.Column>
-            <Grid.Column mobile={16} computer={8}>
-              <BasicTable title="Company" data={companyInfoList} className="no-bottom-border" />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-    );
+    if (
+      !Validator.isEmptyArray(generalInfoList) &&
+      !Validator.isEmptyArray(addressInfoList) &&
+      !Validator.isEmptyArray(companyInfoList)
+    ) {
+      return (
+        <Segment>
+          <Link to="/users"><Icon disabled name='arrow left' />Back to user table</Link>
+          <h1>User Details</h1>
+          <Grid className="fade-in">
+            <Grid.Row>
+              <Grid.Column mobile={16} computer={8}>
+                <BasicTable
+                  title="General"
+                  data={generalInfoList}
+                  className="no-bottom-border"
+                />
+              </Grid.Column>
+            </Grid.Row>
+            <Divider />
+            <Grid.Row>
+              <Grid.Column mobile={16} computer={8}>
+                <BasicTable
+                  title="Address"
+                  data={addressInfoList}
+                  className="no-bottom-border"
+                />
+              </Grid.Column>
+              <Grid.Column mobile={16} computer={8}>
+                <BasicTable
+                  title="Company"
+                  data={companyInfoList}
+                  className="no-bottom-border"
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      );
+    } else {
+      return (
+        <Dimmer active={isLoading} inverted>
+          <Loader size="small">Loading</Loader>
+        </Dimmer>
+      );
+    }
   }
 }
 
